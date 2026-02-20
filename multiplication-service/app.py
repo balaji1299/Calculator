b80542e5f95dc7e740b7be214ea3d18209466b30
balaji1299 <93189228+balaji1299@users.noreply.github.com>
2026-02-20 17:53:19 +0530

    Implement multiplication service with Flask

---
+++ multiplication-service/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/calculate', methods=['POST'])
+def multiply():
+    data = request.get_json()
+    return jsonify({"result": data['a'] * data['b']})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5003)
